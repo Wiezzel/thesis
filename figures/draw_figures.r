679aae45b1784481106139360ddec4bf64229639
Adam Wierzbicki <wiezzel@gmail.com>
2015-05-01 23:35:25 +0200

    General information about SVM and an example figure.

--- figures/draw_figures.r
+++ figures/draw_figures.r
@@ -39,3 +39,45 @@ legend("top", c("linear", "geometric", "weighted"), lty = c("dashed", "dotted", 
        pch = c(16, 16, 16), seg.len = 3, horiz = T, inset = -0.15, bty = "n", xpd = T, 
        text.width = rep(1.7, 3))
 dev.off()
+
+
+## SVM example
+
+tikz("svm.tex", width = 5.25, height = 3)
+par(mar = c(3,3,3,1))
+par(mfrow = c(1,2))
+
+c1_xcoords = c(1,2,2,3,4,5,6)
+c1_ycoords = c(8,5,7,6,9,8,9)
+c2_xcoords = c(5,6,7,8,9)
+c2_ycoords = c(1,2,3,1,3)
+
+plot(c1_xcoords, c1_ycoords, xlab = "", ylab = "", type = "p", pch = 1,
+     xlim = c(0,10), ylim = c(0,10), main = "A", xaxt = "n", yaxt = "n")
+mtext("$x_1$", side = 1, line = 1)
+mtext("$x_2$", side = 2, line = 1)
+points(c2_xcoords, c2_ycoords, pch = 16)
+lines(-1:11, -1:11)
+lines(-1:11, -1:11 + 2, lty = "dashed")
+lines(-1:11, -1:11 - 2, lty = "dashed")
+points(5, 7, pch = 16, col = "white", cex = 1.3)
+points(5, 7, pch = 1, cex = 1.3)
+points(c(5,8), c(3,6), pch = 16, cex = 1.3)
+
+c1_xcoords = c(1,2,2,3,4,5)
+c1_ycoords = c(8,5,7,6,9,8)
+c2_xcoords = c(5,6,7,8,8,9)
+c2_ycoords = c(1,2,3,1,6,3)
+
+plot(c1_xcoords, c1_ycoords, xlab = "", ylab = "", type = "p", pch = 1,
+     xlim = c(0,10), ylim = c(0,10), main = "B", xaxt = "n", yaxt = "n")
+mtext("$x_1$", side = 1, line = 1)
+mtext("$x_2$", side = 2, line = 1)
+points(c2_xcoords, c2_ycoords, pch = 16)
+lines(2:10, 2:10 * 2 -5)
+lines(2:10 - 1, 2:10 * 2 -5, lty = "dashed")
+lines(2:10 + 1, 2:10 * 2 -5, lty = "dashed")
+points(c(5,6), c(7,9), pch = 16, col = "white", cex = 1.3)
+points(c(5,6), c(7,9), pch = 1, cex = 1.3)
+points(5, 3, pch = 16, cex = 1.3)
+dev.off()
